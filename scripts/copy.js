3d0c767aca0ead3cae30769d4f884187152ff685
nowwnik <nik.butorin.2018@bk.ru>
2024-05-22 22:41:53 +0500

    в рот оно того е#ись

---
+++ scripts/copy.js
@@ -0,0 +1,9 @@
+function copyEmail() {
+    navigator.clipboard.writeText
+        ("spkkalina@mail.ru");
+}
+
+function copyPhone() {
+    navigator.clipboard.writeText
+        ("+7(34372) 5-36-91");
+}
---
+++ scripts/swiper.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper(".swiper", {
+  cssMode: true,
+  loop: true,
+  autoplay: {
+    delay: 3000,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
